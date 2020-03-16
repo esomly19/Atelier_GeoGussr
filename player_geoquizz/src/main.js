8b1877d4a23fdd02511bf06955e2c9261941a72e
Luki <lucas.ligerot5@etu.univ-lorraine.fr>
2020-03-16 16:51:02 +0100

    components

--- player_geoquizz/src/main.js
+++ player_geoquizz/src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Game from './components/Game.vue'
+import StartGame from "./components/StartGame.vue"
+import "bootstrap/dist/css/bootstrap.css"
+import 'leaflet/dist/leaflet.css';
+import VueRouter from 'vue-router';
 
-Vue.config.productionTip = false
+Vue.use(VueRouter)
 
+
+const router = new VueRouter({
+  mode: "history",
+
+  routes: [
+    { path: "/", component: StartGame},
+    { path: "/game/:token", component: Game},
+    
+  ]
+})
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router,
+  render: h => h(App)
+})
