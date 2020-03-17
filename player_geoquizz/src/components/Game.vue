b31c2b874e70f0fd5a7ca8b321fa564d1c3ece3e
Luki <lucas.ligerot5@etu.univ-lorraine.fr>
2020-03-17 17:49:48 +0100

    beaucoup de chose

--- player_geoquizz/src/components/Game.vue
+++ player_geoquizz/src/components/Game.vue
@@ -1,19 +1,35 @@
 <template>
-    <div>
-        <maps></maps>
+  <div>
+    <timer></timer>
+    <div class="row game">
+    
+      <maps class="col-8"></maps>
+      <picture-to-find :urlPicture="url" class="col-4"></picture-to-find>
+      <statistiques></statistiques>
+      
     </div>
-  
+    
+    
+  </div>    
 </template>
 <script>
 import Maps from './Maps.vue'
+import PictureToFind from "./PictureToFind.vue"
+import Statistiques from "./Statistiques.vue"
+import Timer from "./Timer.vue"
+
 export default {
   name: 'Game',
   components: {
     Maps,
+    PictureToFind,
+    Statistiques,
+    Timer
   },
   data() {
       return {
-         
+          url: "https://www.nancy-tourisme.fr/app/uploads/nancy/2019/01/place-stanislas-nocturne-2016-7200-rgine-datin-1920x2878.jpg",
+          
       }
     },
 
@@ -26,5 +42,10 @@ export default {
     },
 }
 </script>
+<style>
+  .game{
+    padding: 1%;
+  }
+</style>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 
