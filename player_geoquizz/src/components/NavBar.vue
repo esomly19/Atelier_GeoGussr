bbacdbc5102f8ce307d60ccbdb9af42fb9d026db
Luki <lucas.ligerot5@etu.univ-lorraine.fr>
2020-03-17 11:28:29 +0100

    calcul de distance à partir d'un point

---
+++ player_geoquizz/src/components/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">GeoQuizz</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <router-link to="/"><a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a></router-link>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Classement</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+  
+</template>
+
+<script>
+export default {
+  data() {
+      return {
+         
+      }
+    },
+}
+</script>
