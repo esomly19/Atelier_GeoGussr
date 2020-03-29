1f18663268340bbec0183bf4cdcf9fe99539fade
Luki <lucas.ligerot5@etu.univ-lorraine.fr>
2020-03-29 11:49:47 +0200

    modif api

--- APIs/API_mobile/server.js
+++ APIs/API_mobile/server.js
@@ -391,20 +391,52 @@ app.put("*", (req, res) => {
 
 
 
-const db = mysql.createConnection({
+
+let db = mysql.createConnection({
     host: "db",
     user: "root",
     password: "root",
     database: "geoquizz"
 });
-
-// connexion à la bdd
+/** 
 db.connect(err => {
     if (err) {
         return err;
     }
     console.log("Connected to database");
-})
+})*/
+
+/**let db = mysql.createPool({
+    connectionLimit: 10,
+    host: "db",
+    user: 'root',
+    password:'root',
+    database: 'geoquizz',
+    
+})*/
+
+function startConnection() {
+    console.error('CONNECTING');
+    db = mysql.createConnection({
+        host: "mysql",
+        user: "root",
+        password: "root",
+        database: "geoquizz"
+    });
+    console.log(db)
+    db.connect(function(err) {
+        if (err) {
+            console.error('CONNECT FAILED', err.code);
+            startConnection();
+        }
+        else
+            console.error('CONNECTED');
+    });
+    db.on('error', function(err) {
+        if (err.fatal)
+            startConnection();
+    });
+}
 
 app.listen(PORT, HOST);
 console.log(`API Running on http://${HOST}:${PORT}`)
